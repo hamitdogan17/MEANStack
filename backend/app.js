77410cd7ecaf5ea66eedb764b98caaffdda45825
HAMIT DOGAN <HAMIT.DOGAN@lcwaikiki.com>
2020-09-30 13:43:03 +0300

    mongodb added to project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 const app = express();
+
+mongoose.connect('mongodb+srv://hamit:hamit1234@cluster0.np8qe.mongodb.net/node-angular?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,30 +30,31 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'asdf2332',
-      title: 'First Server-side post',
-      content:'This is coming from server!'
-    },
-    {
-      id: 'asdasdf2332',
-      title: 'Second Server-side post',
-      content:'This is coming from server!'
-    }
-  ]
-  res.status(200).json({
-    message: 'Post fetched succcessfully!',
-    posts: posts
+  Post.find().then(documents => {
+      res.status(200).json({
+        message: 'Post fetched succcessfully!',
+        posts: documents
+    });
+  });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted"});
   });
 });
 
